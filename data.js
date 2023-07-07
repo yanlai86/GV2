0494fcc1eb9c3f3f7ea7aa8e9c06b75fdbb679cb
yanlai86 <136703623+yanlai86@users.noreply.github.com>
2023-07-07 12:27:01 +0100

    Add files via upload

---
+++ data.js
@@ -0,0 +1,278 @@
+var APP_DATA = {
+  "scenes": [
+    {
+      "id": "0-v1",
+      "name": "v1",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 1.5731105285986597,
+        "pitch": -0.004895697526222875,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.8552112224387765,
+          "pitch": 0.10150089572977095,
+          "rotation": 0,
+          "target": "1-v2"
+        },
+        {
+          "yaw": 1.3986604334432862,
+          "pitch": 0.0976212667558265,
+          "rotation": 0,
+          "target": "2-v3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.7156338862839915,
+          "pitch": 0.3136505364165707,
+          "title": "Porada coffee table",
+          "text": "- Dia. 1000mm x 420mmH<div>- Cost: $ 3660</div>"
+        }
+      ]
+    },
+    {
+      "id": "1-v2",
+      "name": "v2",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 2.0111566012948217,
+        "pitch": 0.0000012016462438424469,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5043190030437223,
+          "pitch": 0.0312034639658485,
+          "rotation": 0,
+          "target": "0-v1"
+        },
+        {
+          "yaw": 2.6697299738531113,
+          "pitch": 0.04321672929047615,
+          "rotation": 0,
+          "target": "2-v3"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 1.9446376165795733,
+          "pitch": 0.00664810521277559,
+          "title": "Artwork",
+          "text": "- 1210mmW x 2100mmH"
+        }
+      ]
+    },
+    {
+      "id": "2-v3",
+      "name": "v3",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 1.6842018081308598,
+        "pitch": 0.057662213158856446,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 1.5237433071032482,
+          "pitch": 0.08336179069534566,
+          "rotation": 0,
+          "target": "3-v4"
+        },
+        {
+          "yaw": -1.4893761260293665,
+          "pitch": 0.04772669155452647,
+          "rotation": 0,
+          "target": "0-v1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "3-v4",
+      "name": "v4",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 1.7879233671591717,
+        "pitch": -0.005583094464927996,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 2.3846649376965807,
+          "pitch": 0.02659547706209686,
+          "rotation": 0,
+          "target": "4-v5"
+        },
+        {
+          "yaw": -0.6600370223863035,
+          "pitch": 0.0331756988004237,
+          "rotation": 0,
+          "target": "0-v1"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "4-v5",
+      "name": "v5",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 1.4819899142619795,
+        "pitch": 0.01002255602985258,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.8056803459437205,
+          "pitch": 0.05286480540877214,
+          "rotation": 0,
+          "target": "3-v4"
+        },
+        {
+          "yaw": 0.08671893987084367,
+          "pitch": 0.04559805269741091,
+          "rotation": 0,
+          "target": "5-v6"
+        }
+      ],
+      "infoHotspots": []
+    },
+    {
+      "id": "5-v6",
+      "name": "v6",
+      "levels": [
+        {
+          "tileSize": 256,
+          "size": 256,
+          "fallbackOnly": true
+        },
+        {
+          "tileSize": 512,
+          "size": 512
+        },
+        {
+          "tileSize": 512,
+          "size": 1024
+        }
+      ],
+      "faceSize": 1024,
+      "initialViewParameters": {
+        "yaw": 1.5378250134632534,
+        "pitch": 0.022332377859715535,
+        "fov": 1.3715802068843215
+      },
+      "linkHotspots": [
+        {
+          "yaw": 0.9420993297975748,
+          "pitch": 0.042385255284447254,
+          "rotation": 0,
+          "target": "4-v5"
+        },
+        {
+          "yaw": -0.04785400559538644,
+          "pitch": 0.0290445431693378,
+          "rotation": 0,
+          "target": "3-v4"
+        }
+      ],
+      "infoHotspots": [
+        {
+          "yaw": 2.6269529297787466,
+          "pitch": 0.0765002860793409,
+          "title": "To Outdoor",
+          "text": "Text"
+        },
+        {
+          "yaw": 2.3004115210084812,
+          "pitch": 0.21756058681325996,
+          "title": "Minotti working chair",
+          "text": "- Size:<div>- Finishes:</div><div>- Cost:</div><div>- Delivery:</div>"
+        }
+      ]
+    }
+  ],
+  "name": "Garden Villa",
+  "settings": {
+    "mouseViewMode": "drag",
+    "autorotateEnabled": true,
+    "fullscreenButton": false,
+    "viewControlButtons": false
+  }
+};
